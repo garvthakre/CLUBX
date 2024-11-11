d64c4b7eea3de585480af8d97295eb5619683240
garv thakre <garvthakre0@gmail.com>
2024-11-11 16:18:39 +0530

    setup of express and react

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+function App() {
+  return (
+    
+    <div>
+      <div className='font-extralight bg-black'>hello</div>
+    </div>
+  )
+}
+
+export default App
